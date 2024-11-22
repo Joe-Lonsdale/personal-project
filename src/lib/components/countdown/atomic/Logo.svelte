806ce9e0870294e0c48ead35295a5776c39d729d
Joe Lonsdale <joe.ai.dev@gmail.com>
2024-11-22 16:22:36 +0000

    don't position logo absolutely

--- src/lib/components/countdown/atomic/Logo.svelte
+++ src/lib/components/countdown/atomic/Logo.svelte
@@ -5,16 +5,12 @@
 
 <style>
 	.logo {
-		position: absolute;
-		left: 50%;
-		top: 10%;
 		color: #3982a3;
 		font-family: Karla;
 		font-size: 64px;
 		font-style: normal;
 		font-weight: 700;
 		line-height: normal;
-		transform: translate(-50%, -50%);
 		text-shadow: 0px 5px 12px rgba(0, 0, 0, 0.5);
 	}
 </style>
